090172189a64705ac339c3779e7236c112fe31a7
ahmetulutas <ahmet.ulutas@ecozum.com>
2023-07-18 13:00:38 +0300

    fix: moved some dependencies to dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         muted
       >
       </video>
-      <div className="relative w-80 h-screen flex items-center">
+      <div className="relative w-80 h-screen flex items-center p-4">
         <Image alt="camera-focus" style={{ width: '100%', height: 'auto', objectFit: 'cover' }} width={400} height={400} src={'/camera.png'} className="m-auto p-4" />
       </div>
       <div className="fixed top-0 h-full w-full bg-overLay -z-[8] opacity-70">
@@ -21,7 +21,7 @@ export default function Home() {
       <Container className={'flex justify-between text-sm md:text-xl py-4 items-center'}>
         <p>OBLOMOV</p>
         <div className="relative w-20 h-10">
-          <Image alt="slider-bar" fill src={'/slider-bar.png'} />
+          <Image alt="slider-bar" fill src={'/slider-bar.png'} style={{ objectFit: 'cover' }} />
         </div>
         <Link href="/" className="flex gap-2 items-center">
           <p>Daha fazla</p>
--- src/components/containers/Container.tsx
+++ src/components/containers/Container.tsx
@@ -1,4 +1,4 @@
 import React, { HTMLProps } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export const Container = ({ className, ...props }: HTMLProps<HTMLDivElement>) => <div className={twMerge('mx-auto w-[98%] px-4', className)} {...props} />;
+export const Container = ({ className, ...props }: HTMLProps<HTMLDivElement>) => <div className={twMerge('mx-auto w-[96%] px-4', className)} {...props} />;
