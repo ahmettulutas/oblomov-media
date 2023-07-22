0824572366902caf12755b77f237acd379a9e3c1
ahmetulutas <ahmet.ulutas@ecozum.com>
2023-07-22 18:27:44 +0300

    add: added temporary banner and removed gltf from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,32 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
-import { CameraCanvas } from '@/components/3dCamera';
 import { Container } from '@/components/containers';
 
 export default function Home() {
   return (
-    <main className={'flex h-screen flex-col items-center justify-between bg-secondary'}>
-      <Container className={'fixed bottom-0 left-0 right-0 flex justify-between text-sm md:text-xl py-4 items-center z-10'}>
+    <main className={'flex h-screen max-h-screen flex-col items-center justify-between'}>
+      <video className="fixed top-0 h-full w-full object-cover -z-[10] overflow-hidden"
+        src="/banner.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="relative w-80 h-screen flex items-center">
+        <Image alt="camera-focus" style={{ width: '100%', height: 'auto', objectFit: 'cover' }} width={400} height={400} src={'/camera.png'} className="m-auto p-4" />
+      </div>
+      <div className="fixed top-0 h-full w-full bg-overLay -z-[8] opacity-70" />
+      <Container className={'flex justify-between text-sm md:text-xl py-4 items-center'}>
         <p>OBLOMOV</p>
         <div className="relative w-20 h-10">
-          <Image alt="slider-bar" fill src={'/slider-bar.png'} style={{ objectFit: 'cover' }} />
+          <Image alt="slider-bar" fill src={'/slider-bar.png'} />
         </div>
         <Link href="/" className="flex gap-2 items-center">
           <p>Daha fazla</p>
           <HiArrowNarrowRight />
         </Link>
       </Container>
-      <CameraCanvas />
+      {/*       <CameraCanvas /> */}
     </main>
   );
 }
